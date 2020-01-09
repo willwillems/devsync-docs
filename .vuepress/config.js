6bb352dab699dd6d7c9fc215f6badf83981f675f
Rutger Willems <rut.willems@gmail.com>
2020-01-09 21:31:46 +0100

    add video loading to markdown

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,18 @@
 module.exports = {
   title: 'Devsync Documentation',
   description: 'Documentation for Devsync.',
-  dest: 'dist'
+  dest: 'dist',
+  markdown: {
+    extendMarkdown: md => {
+      md.use(require('markdown-it-html5-embed'), {
+        html5embed: {
+          useImageSyntax: true,
+          attributes: {
+            'audio': 'autoplay loop muted style="width: 480px; border-radius: 5px;"',
+            'video': 'autoplay loop muted style="width: 480px; border-radius: 5px;"'
+          }
+        },
+      })
+    }
+  }
 }
