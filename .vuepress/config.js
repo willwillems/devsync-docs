dacf0f2e20b64585ae9dbf3b5dc70f65ac866456
Rutger Willems <rut.willems@gmail.com>
2020-01-13 13:37:30 +0100

    fix video's spilling outside viewport on mobile

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,8 +18,8 @@ module.exports = {
         html5embed: {
           useImageSyntax: true,
           attributes: {
-            'audio': 'autoplay loop muted style="width: 480px; border-radius: 5px;"',
-            'video': 'autoplay loop muted style="width: 480px; border-radius: 5px;"'
+            'audio': 'autoplay loop muted style="width: 480px; max-width: 100%; border-radius: 5px;"',
+            'video': 'autoplay loop muted style="width: 480px; max-width: 100%; border-radius: 5px;"'
           }
         },
       })
