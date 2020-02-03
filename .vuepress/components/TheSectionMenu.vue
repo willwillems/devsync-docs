3c4f8d1c0e892cf15c3ff00126761458857024c9
Rutger Willems <rut.willems@gmail.com>
2020-02-03 14:07:07 +0200

    add quick link section

---
+++ .vuepress/components/TheSectionMenu.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="section-menu">
+    <a class="section-menu-item" v-for="section in sections" :href="section.link" >
+      <div class="section-menu-item__bar"></div>
+      <h1 class="section-menu-item__title">{{ section.title }}</h1>
+      <p class="section-menu-item__text">{{ section.description }}</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sections: [
+        {
+          title: 'Quickstart',
+          description: 'Get Devsync running and learn the basic controls.',
+          link: '/quickstart'
+        },
+        {
+          title: 'Troubleshooting',
+          description: 'Find out what is wrong, probably sourcemaps.',
+          link: '/troubleshooting'
+        },
+        {
+          title: 'Compatibility',
+          description: 'Check compatibility status and common problems.',
+          link: '/compatibility'
+        },
+        {
+          title: 'Contact',
+          description: 'Didn\'t find what you were looking for?',
+          link: 'https://devsync.co'
+        },
+      ]
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.section-menu {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px -10px;
+}
+
+.section-menu-item {
+  flex-basis: 340px;
+  padding: 10px;
+  flex-grow: 1;
+  color: inherit;
+
+  &__bar {
+    width: 100%;
+    height: 7px;
+  }
+
+
+  &__title {
+    margin: .2em 0;
+    font-size: 1.5rem;
+  }
+
+  &__text {
+    margin: .4em 0;
+  }
+
+  &:nth-child(1) .section-menu-item__bar {
+    background-color: #70DABC
+  }
+
+  &:nth-child(2) .section-menu-item__bar {
+    background-color: #F74C71
+  }
+
+  &:nth-child(3) .section-menu-item__bar {
+    background-color: #FFA75F
+  }
+
+  &:nth-child(4) .section-menu-item__bar {
+    background-color: #5FE4FF
+  }
+}
+
+</style>
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 These are the docs for [Devsync](https://devsync.co).
 
-## Tips
-- Use `CMD`/`CTRL`+`OPTION`/`SHIFT`+`C` to open the elements devtools panel and then `CMD`/`CTRL`+`[` to navigate to the last devtools panel.
+<TheSectionMenu/>
+
+<!-- ## Tips
+- Use `CMD`/`CTRL`+`OPTION`/`SHIFT`+`C` to open the elements devtools panel and then `CMD`/`CTRL`+`[` to navigate to the last devtools panel. -->
